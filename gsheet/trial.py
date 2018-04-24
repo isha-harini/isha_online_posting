29b8a3e8c00d4910d1ee30a83814180392755700
Harini Muthukrishnan <isha.harini.umich@gmail.com>
2018-04-24 13:04:27 -0400

    Reading event details from GSheet
    Sample event read from Gsheet works

--- gsheet/trial.py
+++ gsheet/trial.py
@@ -10,9 +10,33 @@ gc = gspread.authorize(credentials)
 
 testsheet = gc.open("Online_posting_test_sheet").sheet1
 
-#get value
-val = testsheet.acell('A1').value
-val2 = testsheet.acell('A2').value
+#event_column = raw_input("Enter the ColumnID of the event in the spreadsheet (e.g. AH)")
 
-print 'A1 is ' + val
-print 'A2 is ' + val2
+event_column = 'A'
+
+#hardcoding row numbers for various details
+r_evname = 1
+r_evday = 2
+r_evdate = 3
+r_evtime = 4
+r_evloc = 5
+r_evaddr = 6
+r_evcity = 7
+r_evzip = 8
+
+evname = testsheet.acell(event_column + str(r_evname)).value
+evday = testsheet.acell(event_column + str(r_evday)).value
+evdate = testsheet.acell(event_column + str(r_evdate)).value
+evtime = testsheet.acell(event_column + str(r_evtime)).value
+evloc = testsheet.acell(event_column + str(r_evloc)).value
+evaddr = testsheet.acell(event_column + str(r_evaddr)).value
+evcity = testsheet.acell(event_column + str(r_evcity)).value
+evzip = testsheet.acell(event_column + str(r_evzip)).value
+
+print evname
+print evday
+print evtime
+print evloc
+print evaddr
+print evcity
+print evzip
