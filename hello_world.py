eb5ecd797ead91877872b0b50a5adad3216b9bc2
Harini Muthukrishnan <isha.harini.umich@gmail.com>
2018-04-25 17:04:38 -0400

    Hello world scripts to test installation

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common import action_chains, keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import re
+import time
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from datetime import datetime
+
+print "Test successful"
---
+++ hello_world_gsheet.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common import action_chains, keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import re
+import time
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from datetime import datetime
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('gsheet/IshaOnlinePosting-f601765b7565.json', scope)
+
+gc = gspread.authorize(credentials)
+
+testsheet = gc.open("Online_posting_test_sheet").sheet1
+
+print "Test successful"
