59b520497abfa7016328028df3a109df5c6ddb91
Harini Muthukrishnan <isha.harini.umich@gmail.com>
2018-04-25 13:46:33 -0400

    Started GSHEET integration

--- online_posting.py
+++ online_posting.py
@@ -36,6 +36,7 @@ eb_password = 'Silver123'
 
 ################## FLAGS ######################
 DEBUG = 0 # 1 - Enable DEBUG; 0 - Disable DEBUG
+
 GSHEET = 0 # 1 - Enable input read from Google Sheet; 0 - Disable input read from Google Sheet
 
 ################# EVENTBRITE ###########################
@@ -132,7 +133,43 @@ if DEBUG == 0 and GSHEET == 0:
 		exit()
 
 #TODO: GSheet integration
+if GSHEET == 1:
+	import gspread
+	from oauth2client.service_account import ServiceAccountCredentials
 
+	scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('IshaOnlinePosting-f601765b7565.json', scope)
+
+	gc = gspread.authorize(credentials)
+
+	testsheet = gc.open("Online_posting_test_sheet").sheet1
+	testsheet = gc.open("Online_posting_test_sheet").sheet1
+
+	#event_column = raw_input("Enter the ColumnID of the event in the spreadsheet (e.g. AH)")
+
+	event_column = 'A'
+
+	#hardcoding row numbers for various details
+	r_evname = 1
+	r_evday = 2
+	r_evdate = 3
+	r_evtime = 4
+	r_evloc = 5
+	r_evaddr = 6
+	r_evcity = 7
+	r_evzip = 8
+	
+	evname = testsheet.acell(event_column + str(r_evname)).value
+	evday = testsheet.acell(event_column + str(r_evday)).value
+	evdate = testsheet.acell(event_column + str(r_evdate)).value
+	evtime = testsheet.acell(event_column + str(r_evtime)).value
+	evloc = testsheet.acell(event_column + str(r_evloc)).value
+	evaddr = testsheet.acell(event_column + str(r_evaddr)).value
+	evcity = testsheet.acell(event_column + str(r_evcity)).value
+	evzip = testsheet.acell(event_column + str(r_evzip)).value
+	
 
 #actual event
 
